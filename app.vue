e0ba795be7ada15d7c522832eeb2b63b7278362c
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-25 14:07:30 +0900

    htmlタグのlang属性をjaに変更し、descriptionを記述

--- app.vue
+++ app.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+useHead({
+  title: "Nuxt3BlogApp",
+  meta: [
+    { name: "description", content: "BlogApp with Nuxt.js3" }
+  ],
+  htmlAttrs: {
+    lang: "ja",
+  },
+})
+</script>
+
 <template>
   <div>
     <NuxtWelcome />
