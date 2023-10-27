c4066aa4f9b22100efa5ad02bb00d001078a422b
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-27 21:33:34 +0900

    ユーザー登録フォームのマークアップとスタイリングを実装 (#1)

---
+++ components/organisms/SignupForm/SignupForm.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <form class="h-full flex items-center justify-center text-center">
+    <fieldset class="border rounded-2xl grow">
+      <legend class="px-2">登録</legend>
+      <label>
+        名前
+        <input type="text" name="name" id="name" class="border block mx-auto" />
+      </label>
+
+      <label>
+        メールアドレス
+        <input type="email" name="email" id="email" class="border block mx-auto" />
+      </label>
+
+      <label>
+        パスワード
+        <input type="password" name="password" id="password" class="border block mx-auto" />
+      </label>
+
+      <label>
+        パスワード確認
+        <input type="password" name="password_confirmation" id="password_confirmation" class="border block mx-auto" />
+      </label>
+
+      <button>登録</button>
+    </fieldset>
+  </form>
+</template>
---
+++ pages/signup/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useHead({
+  title: "ユーザー登録フォーム - Nuxt3BlogApp",
+})
+</script>
+
+<template>
+  <SignupForm />
+</template>
