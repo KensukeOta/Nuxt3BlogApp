f2ee7c5857186b63bb9c0372c623d338a32238d9
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-28 21:38:47 +0900

    ログインフォームのマークアップとスタイリングを実装 (#3)

---
+++ components/organisms/LoginForm/LoginForm.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <form class="h-full flex items-center justify-center text-center">
+    <fieldset class="border rounded-2xl grow">
+      <legend class="px-2">ログイン</legend>
+
+      <label>
+        メールアドレス
+        <input type="email" name="email" id="email" class="border block mx-auto" />
+      </label>
+
+      <label>
+        パスワード
+        <input type="password" name="password" id="password" class="border block mx-auto" />
+      </label>
+
+      <button>ログイン</button>
+    </fieldset>
+  </form>
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+useHead({
+  title: "ログインフォーム - Nuxt3BlogApp",
+})
+</script>
+
+<template>
+  <LoginForm />
+</template>
