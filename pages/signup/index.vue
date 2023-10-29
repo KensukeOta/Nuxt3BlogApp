dc846c2530e87c187976da364940b96b2a834199
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-10-29 16:28:12 +0900

    ログイン時にユーザー登録フォームやログインフォームに遷移しようとしたらトップページにリダイレクトするよう実装 (#3)

--- pages/signup/index.vue
+++ pages/signup/index.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts">
+import type { authUser } from '~/types/authUser';
+
 useHead({
   title: "ユーザー登録フォーム - Nuxt3BlogApp",
 })
+
+onMounted(async() => {
+  const runtimeConfig = useRuntimeConfig();
+  
+  const { data, pending, error, refresh } = await useFetch<authUser>(`${runtimeConfig.public.apiUrl}/v1/user`, {
+    headers: {
+      "Accept": "application/json",
+    },
+    credentials: "include"
+  });
+  await refresh();
+  
+  if (data.value?.authUser) {
+    await navigateTo("/");
+  }
+});
 </script>
 
 <template>
